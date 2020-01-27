d985864a83c5741e597c938f5a393532c38036e8
Rachel4858 <rachel458458@gmail.com>
2020-01-27 17:25:41 +0900

    refactoring validParentheses.js

--- LeetCode/validParentheses.js
+++ LeetCode/validParentheses.js
@@ -1,4 +1,4 @@
-const strA = "()[]{}}";
+const strA = "()[]{[]}";
 const strB = "{[]}";
 
 function validParentheses(bracketStr) {
@@ -14,10 +14,17 @@ function validParentheses(bracketStr) {
         if (bracketArray[i] === '(' || bracketArray[i] === '[' || bracketArray[i] === '{') {
             tmp.push(bracketArray[i]);
 
-        } else if (bracketMap.get(tmp[tmp.length - 1]) === bracketArray[i]) {
-            tmp.pop();
-        }
+        } else {
+            if (tmp.length === 0) {
+                return false;
+            }
 
+            if (bracketMap.get(tmp[tmp.length - 1]) === bracketArray[i]) {
+                tmp.pop();
+            } else {
+                return false;
+            }
+        }
     }
 
     if(tmp.length === 0) {
