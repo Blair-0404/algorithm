dac4fa7f0e3570ff9a5b9c2a573782dfa937aae9
Rachel4858 <rachel458458@gmail.com>
2020-02-02 22:32:44 +0900

    add code programmers-exam.js

--- progorammers/exam.js
+++ progorammers/exam.js
@@ -1,27 +1,38 @@
 // 완전탐색
 
 function solution(answers) {
-    let result = [0, 0, 0];
-    const routine = [[1, 2, 3, 4, 5], [2, 1, 2, 3, 2, 4, 2, 5], [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
-    // let array = [{1: 0}, {2: 0}, {3: 0}];
+    let result = { 1: 0, 2: 0, 3: 0 };
+    const routine = [[1, 2, 3, 4, 5], [2, 1, 2, 3, 2, 4, 2, 5], [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]];
 
-    for (let i = 0; i < answer.length - 1; i++) {
-        for (let j = 0; j < routine.length - 1; j++) {
-            while (answers.length !== 0) {
-                if (routine[j].length <= answers.length) {
-                    if (routine[j][i] === answers[i]) {
-                        result[j] += 1;
-                    }
-                } else {
+    for (let i = 0; i < answers.length; i++) {
+        for (let j = 0; j < routine.length; j++) {
+            if (routine[j][i % routine[j].length] === answers[i]) {
+                result[j + 1] = result[j + 1] + 1;
+            }
+        }
+    }
 
-                }
+    let max = -1;
+    return Object.entries(result).sort((first, second) => {
+            const comp = parseInt(second[1]) - parseInt(first[1]);
+            if (comp === 0) {
+                return parseInt(first[0]) - parseInt(second[0]);
             }
 
-        }
-        return result;
-    }
+            return comp;
+        }).filter((item) => {
+            if (item[1] === 0) {
+                return false;
+            } else if (max === -1) {
+                max = item[1];
+                return true;
+            } else if (item[1] === max) {
+                return true;
+            }
+
+            return false;
+        }).map((item) => parseInt(item[0]));
 }
 
-
-const answer = [1, 2, 3, 4, 5];
+const answer = [1,3,2,4,2];
 console.log(solution(answer));
