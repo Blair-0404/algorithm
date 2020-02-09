020c8d73109d47bbf732b22e5054cf7d1551f81e
Rachel4858 <rachel458458@gmail.com>
2020-02-09 20:31:20 +0900

    Coding 12928.js

---
+++ programmers/12928.js
@@ -0,0 +1,14 @@
+function solution(n) {
+  let result = 0;
+
+  for(let i = 1; i <= Math.sqrt(n); i++) {
+    if(n % i === 0) {
+      if (i !== Math.sqrt(n)) result += (n / i);
+      result += i;
+    }
+  }
+
+  return result;
+}
+
+console.log(solution(25));
