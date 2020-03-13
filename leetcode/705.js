84256970a380489c9fb5e9928d54ca4c273e1986
Rachel4858 <rachel458458@gmail.com>
2020-03-13 15:21:36 +0900

    Refectoring LeetCode Easy-705.js

--- leetcode/705.js
+++ leetcode/705.js
@@ -1,28 +1,43 @@
-class HashSet {
-  constructor() {
-    this.arr = [];
-    this.isVal = false;
+class MyHashSet {
+
+  arr = [];
+  isVal = false;
+
+  add(val) {
+    this.arr.push(val);
   }
 
-  add(key) {
-    this.arr.push(key);
-    console.log(this.arr);
-  }
+  remove(val) { // val은 다 사라져야 했다.!   더 짧아질수는 없을까..
+    let tmpCount = 0;
+    let tmpIdx = 0;
+    this.arr.sort((a, b) => {
+      return a - b
+    });
 
-  remove(key) {
-    for(let i = 0; i < this.arr.length; i++) {
-      if(key === this.arr[i]) {
-        this.arr.splice(i, 1)
+    for (let i = 0; i < this.arr.length; i++) {
+      if (val === this.arr[i]) {
+        tmpIdx = i;
+        break
       }
     }
-    console.log(this.arr);
+
+    for (let j = 0; j < this.arr.length; j++) {
+      if (val === this.arr[j]) {
+        tmpCount++;
+      }
+    }
+
+    this.arr.splice(tmpIdx, tmpCount);
   }
 
-  contains(key) {
-    for(let i = 0; i < this.arr.length; i++) {
-      if(key === this.arr[i]) {
-        this.isVal = true;
-        break
+  contains(val) {
+    if (!this.arr.length) {
+      this.isVal = false;
+    }
+
+    for (let i = 0; i < this.arr.length; i++) {
+      if (val === this.arr[i]) {
+        return true
       }
     }
     return this.isVal;
@@ -30,7 +45,14 @@ class HashSet {
 
 }
 
-const MyHashSet = new HashSet();
-MyHashSet.add(2);
-MyHashSet.add(3);
-MyHashSet.contains(2);
+const HashSet = new MyHashSet();
+
+HashSet.add(2);
+HashSet.add(1);
+console.log(HashSet.contains(1));
+console.log(HashSet.contains(3));
+HashSet.add(2);
+console.log(HashSet.contains(2));
+HashSet.remove(2);
+console.log(HashSet.contains(2));
+console.log(HashSet);
