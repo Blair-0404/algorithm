e2d21592ae35bf46a8d05546efe54455ee25bcb6
Rachel4858 <rachel458458@gmail.com>
2020-06-01 19:11:57 +0900

    leetcode-amazon

--- leetcode/amazon/linkedList/3-ReverseNodesink-Group.js
+++ leetcode/amazon/linkedList/3-ReverseNodesink-Group.js
@@ -2,8 +2,6 @@
 // For k = 2, you should return: 2->1->4->3->5
 // For k = 3, you should return: 3->2->1->4->5
 
-
-
 var reverseKGroup = function(head, k) {
   const root = new ListNode();
   root.next = head;
@@ -14,7 +12,7 @@ var reverseKGroup = function(head, k) {
     head = head.next;
     i += 1;
     if (i === k) {
-      const [reversedHead, reversedTail] = reverse(prev.next, k);
+      const [reversedHead, reversedTail] = reverse(prev.next, k); // reversedHead = 2, reversedTail = 1
       prev.next = reversedHead;
       reversedTail.next = head;
       prev = reversedTail;
@@ -25,7 +23,7 @@ var reverseKGroup = function(head, k) {
   return root.next;
 };
 
-function reverse(curr, length) {
+function reverse(curr, length) { // 1<-2<-3->4->5로 뒤집는 거라면 1노드부터 2번 화살표를 바꿔야한다 => 즉 인자는 (1,2) 노드1부터, 2번 화살표바꾸기
   const tail = curr;
   let prev = null;
   while (length > 0) {
@@ -35,5 +33,41 @@ function reverse(curr, length) {
     curr = next; // 4. 담아뒀던 다음 노드는 현재 노드가 되기
     length -= 1;
   }
-  return [prev, tail];
+  return [prev, tail]; // 결과는 prev = 화살표 바꾸기가 끝난 노드 tail = 인풋의 첫인자(시작노드)
+                       // 즉 함수의 첫인자가 이함수의 리턴값 tail이 된다.
+// }
+
+
+const reverseKGroup = function(head, k) {
+  const root = new ListNode(-1);
+  root.next = head;
+
+  //
+  let i = 0;
+
+  while(head) {
+    if(i % k === 0) {
+      //
+    } else {
+      head = head.next;
+    }
+    i++;
+  }
+
+  return root.next;
+};
+
+const reverse = function(curr, count) {
+  const end = curr;
+  let prev = null;
+
+  while(count > 0) {
+    const next = curr.next;
+    curr.next = prev;
+
+    prev = curr;
+    curr = next;
+    count--;
+  }
+  return end
 }
